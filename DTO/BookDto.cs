be383ad4d98bbb11be276d64b6bf990ccf32b3b5
Asim <shahasim190@gmail.com>
2024-04-21 09:05:13 +0500

    Dto files

---
+++ DTO/BookDto.cs
@@ -0,0 +1,16 @@
+﻿namespace DTO
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string Genre { get; set; }
+
+        public DateTime PublicationYear { get; set; } = DateTime.Now;
+        public string ISBN { get; set; }
+        public decimal Price { get; set; } = decimal.Zero;
+        public int Quantity { get; set; } = 0;
+    }
+}
